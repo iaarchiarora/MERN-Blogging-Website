fac40ecb1e7bbab46a00a6873ac5d0408f0c2763
Aarchi Arora <aarchiarora2003@gmail.com>
2024-05-07 01:05:42 +0530

    Commit Pages and Push

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+
+import {BrowserRouter, Routes,Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Dashboard from './Pages/Dashboard'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import Projects from './Pages/Projects'
+
+
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/about" element={<About/>}></Route>
+      <Route path="/dashboard" element={<Dashboard/>}></Route>
+      <Route path="/signin" element={<SignIn/>}></Route>
+      <Route path="/signup" element={<SignUp/>}></Route>
+      <Route path="/projects" element={<Projects/>}></Route>
+      </Routes>
+   
+    </BrowserRouter>
   )
 }
