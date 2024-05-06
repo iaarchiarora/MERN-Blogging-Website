b044366651140fc46e08835dcf4f36fbd9a0292b
Aarchi Arora <aarchiarora2003@gmail.com>
2024-05-06 10:45:23 +0530

    install react js and tailwind css

---
+++ client/tailwind.config.js
@@ -0,0 +1,24 @@
+//from tailwind website
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
