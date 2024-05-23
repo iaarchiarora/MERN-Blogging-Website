f5a91461e4ff36fd050a742dce46a2826941885f
Aarchi Arora <aarchiarora2003@gmail.com>
2024-05-23 21:47:41 +0530

    Connect to the databased server

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import mongoose from 'mongoose';
+mongoose.connect
 const app = express();
 app.listen(3000,() => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000!');
 });
